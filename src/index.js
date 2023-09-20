c8c27b27cda24af792a13ed991b99538bfc00106
amine abaidi <amineabaidi546@gmail.com>
2023-09-20 16:24:25 +0100

    The app now is PWA

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,3 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
+serviceWorkerRegistration.register();
